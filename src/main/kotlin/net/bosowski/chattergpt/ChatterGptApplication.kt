199d26ae2fe9e2b169cb541e368c20c8b83b6544
Bartosz Osowski <bartosz.osowski@errigal.com>
2023-01-19 23:12:09 +0000

    Initial commit.

---
+++ src/main/kotlin/net/bosowski/chattergpt/ChatterGptApplication.kt
@@ -0,0 +1,60 @@
+package net.bosowski.chattergpt
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpStatus
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.security.oauth2.core.user.OAuth2User
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.HttpStatusEntryPoint
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+import java.util.*
+
+
+@SpringBootApplication
+@RestController
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
+class ChatterGptApplication {
+
+    @GetMapping("/user")
+    fun user(@AuthenticationPrincipal principal: OAuth2User): Map<String?, Any?> {
+        return Collections.singletonMap("name", principal.getAttribute("name"))
+    }
+
+    @Bean
+    @Throws(Exception::class)
+    fun authenticationManager(authenticationConfiguration: AuthenticationConfiguration): AuthenticationManager {
+        return authenticationConfiguration.authenticationManager
+    }
+
+    @Bean
+    @Throws(java.lang.Exception::class)
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .authorizeHttpRequests().antMatchers("/", "/error", "/logout", "/webjars/**").permitAll().anyRequest()
+            .authenticated().and()
+            .logout().logoutSuccessUrl("/").permitAll().and()
+            .exceptionHandling().authenticationEntryPoint(HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)).and()
+            .oauth2Login().and()
+            .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+
+        return http.build()
+    }
+}
+
+fun main(args: Array<String>) {
+    runApplication<ChatterGptApplication>(*args)
+}
---
+++ src/test/kotlin/net/bosowski/chattergpt/ChatterGptApplicationTests.kt
@@ -0,0 +1,13 @@
+package net.bosowski.chattergpt
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ChatterGptApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
