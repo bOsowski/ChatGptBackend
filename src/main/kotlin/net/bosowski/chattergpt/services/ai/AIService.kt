ba8fbb4d6b71778303105dacd00a1c0fb53f00c1
Bartosz Osowski <bartosz.osowski@errigal.com>
2023-02-05 21:40:30 +0000

    Now returning a ResponseEntity that can have different return codes.

--- src/main/kotlin/net/bosowski/chattergpt/services/ai/AIService.kt
+++ src/main/kotlin/net/bosowski/chattergpt/services/ai/AIService.kt
@@ -1,17 +1,23 @@
 package net.bosowski.chattergpt.services.ai
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import net.bosowski.chattergpt.data.models.authentication.OauthUser
 import net.bosowski.chattergpt.data.repositories.authentication.UserRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.ResponseEntity
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient
+import org.springframework.security.oauth2.common.exceptions.UnauthorizedClientException
 import org.springframework.security.oauth2.jwt.Jwt
 import org.springframework.stereotype.Service
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.client.HttpClientErrorException.Unauthorized
+import org.springframework.web.servlet.view.RedirectView
 import java.net.URI
 import java.net.http.HttpClient
 import java.net.http.HttpRequest
@@ -28,10 +34,13 @@ class AIService {
     private lateinit var userRepository: UserRepository
 
     @GetMapping("/api/ai")
-    fun getResponse(@AuthenticationPrincipal jwt: Jwt): String {
+    fun getResponse(@AuthenticationPrincipal jwt: Jwt): ResponseEntity<String> {
         val email = jwt.claims["email"] as String
         val oauthUser = userRepository.findByUsername(email)
-        return "Hello"
+        if(oauthUser == null){
+            return ResponseEntity("User not registered.", org.springframework.http.HttpStatus.UNAUTHORIZED)
+        }
+        return ResponseEntity("Hello world!", org.springframework.http.HttpStatus.OK)
     }
 
 }
