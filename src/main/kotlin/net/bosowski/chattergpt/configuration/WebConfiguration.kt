506f9d19ddb3d8013709b9240cef30eb4765904f
Bartosz Osowski <bartosz.osowski@errigal.com>
2023-02-16 13:00:13 +0000

    Removed the unneeded authenticationManager class
    This fixed the stackoverflow error on failed authentications.

--- src/main/kotlin/net/bosowski/chattergpt/configuration/WebConfiguration.kt
+++ src/main/kotlin/net/bosowski/chattergpt/configuration/WebConfiguration.kt
@@ -2,38 +2,21 @@ package net.bosowski.chattergpt.configuration
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.core.Ordered
 import org.springframework.core.Ordered.HIGHEST_PRECEDENCE
 import org.springframework.core.annotation.Order
-import org.springframework.core.convert.converter.Converter
 import org.springframework.http.HttpStatus
-import org.springframework.security.authentication.AbstractAuthenticationToken
-import org.springframework.security.authentication.AuthenticationManager
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-import org.springframework.security.core.GrantedAuthority
-import org.springframework.security.core.authority.SimpleGrantedAuthority
-import org.springframework.security.oauth2.jwt.Jwt
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken
-import org.springframework.security.web.FilterChainProxy
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.HttpStatusEntryPoint
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository
-import java.util.stream.Stream
 
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 class WebConfiguration {
-
-    @Bean
-    @Throws(Exception::class)
-    fun authenticationManager(authenticationConfiguration: AuthenticationConfiguration): AuthenticationManager {
-        return authenticationConfiguration.authenticationManager
-    }
 
     @Bean
     @Order(HIGHEST_PRECEDENCE)
