0189e6251007c42b6a9b3d9d755f19a181175006
Bartosz Osowski <bartosz.osowski@errigal.com>
2023-04-16 20:38:55 +0100

    Improved autocompletions - they are now longer. Reformatted code.

--- src/main/kotlin/net/bosowski/chattergpt/controllers/api/AI.kt
+++ src/main/kotlin/net/bosowski/chattergpt/controllers/api/AI.kt
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.client.RestTemplate
+import java.lang.Exception
 import java.util.*
 import javax.servlet.http.HttpServletRequest
 
@@ -38,7 +39,7 @@ class AI {
     private lateinit var modelRequestRepository: ModelRequestRepository
 
     @PostMapping("/chatRequest")
-    fun chatRequest(@AuthenticationPrincipal jwt: Jwt, @RequestBody chatRequestDto: ChatRequestDto): ResponseEntity<String> {
+    fun chatRequest(@AuthenticationPrincipal jwt: Jwt, @RequestBody chatRequestDto: ChatRequestDto): ResponseEntity<Any> {
         val email = jwt.claims["email"] as String
         val user = userRepository.findByUsername(email)
         if(user == null) {
@@ -46,7 +47,9 @@ class AI {
         }
 
         val participants = chatRequestDto.messages.map { it.sender }.distinct().toMutableList()
-        val prompt = """The below chat is between me and ${participants.filter { it != "Me" }.joinToString(",") }. Create a response as if you were me. Don't use much punctuation and keep in mind that today's date is ${Date()}.
+        val prompt = """The below chat is between me and ${
+            participants.filter { it != "Me" }.joinToString(",")
+        }. Create a response as if you were me. Don't use much punctuation and keep in mind that today's date is ${Date()}.
             
 ${chatRequestDto.messages.joinToString(separator = "\n") { "${it.sender}:${it.message}" }}
 Me:"""
@@ -59,47 +62,30 @@ Me:"""
         modelRequest.maxTokens = chatRequestDto.maxTokens
         modelRequest.temperature = chatRequestDto.temperature
         modelRequestRepository.save(modelRequest)
-        return getResponseFromModelRequest(modelRequest)
+        val apiResponse = performRequest(modelRequest)
+        return ResponseEntity(apiResponse.body, HttpStatus.OK)
     }
 
 
     @PostMapping("/autocompleteRequest")
-    fun getResponse(@AuthenticationPrincipal jwt: Jwt, @RequestBody text: String): ResponseEntity<String> {
+    fun getResponse(@AuthenticationPrincipal jwt: Jwt, @RequestBody text: String): ResponseEntity<Any> {
         val email = jwt.claims["email"] as String
         val user = userRepository.findByUsername(email) ?: return ResponseEntity("User not registered.", HttpStatus.UNAUTHORIZED)
         val modelRequest = ModelRequest()
-        modelRequest.prompt = "The following is a user input inside of a text field. The autocomplete provides predictions of what the user wants to type.\n" +
-                              "\n" +
-                              "UserInput:Hello, who ar\n" +
-                              "AIAutocomplete:e you?\n" +
-                              "UserInput:"+text+"\n" +
-                              "AIAutocomplete:"
-        modelRequest.stop = mutableListOf("AIAutocomplete:", "UserInput:")
+        modelRequest.prompt =
+            "The user is typing text into a text field on his phone. Your replies should only consist of the prediction. Be creative and try to predict the full contents of the text field, given the following message that is wrapped in quotes:\"${text}\""
         modelRequest.oauthUser = user
+        modelRequest.temperature = 0.6f
+        modelRequest.n = 3
         modelRequestRepository.save(modelRequest)
-        return getResponseFromModelRequest(modelRequest)
-//        val response = getResponseFromModelRequest(modelRequest)
-//        return  ResponseEntity(response.body!!.replace("\n", "").replace("\n", ""), response.statusCode)
-    }
-
-    private fun getResponseFromModelRequest(modelRequest: ModelRequest): ResponseEntity<String> {
-        if(modelRequest.oauthUser!!.availableCredits < getMaxCost(modelRequest)){
-            return getNotEnoughCreditsResponse()
-        }
-        val response = performRequest(modelRequest)
-        if(response.body == null ){
-            return ResponseEntity("Invalid response from OpenAI.", HttpStatus.NO_CONTENT)
-        }
-
-        val apiResponseDto: ApiResponseDto = response.body!!
-        return ResponseEntity(apiResponseDto.choices?.first()?.text, HttpStatus.OK)
-    }
-
-    private fun getNotEnoughCreditsResponse(): ResponseEntity<String>{
-        return ResponseEntity("Not enough credits.", HttpStatus.PAYMENT_REQUIRED)
+        val apiResponse = performRequest(modelRequest)
+        return ResponseEntity(apiResponse.body, HttpStatus.OK)
     }
 
     private fun performRequest(modelRequest: ModelRequest): ResponseEntity<ApiResponseDto> {
+        if(modelRequest.oauthUser !!.availableCredits < getMaxCost(modelRequest)) {
+            throw Exception("Not enough credits!") //todo: create an exception class for this particular exception
+        }
         val restTemplate = RestTemplate()
         val headers = HttpHeaders()
         headers.add("Authorization", "Bearer $openaiApiKey")
@@ -108,14 +94,14 @@ Me:"""
         val response = restTemplate.postForEntity(openaiApiUrl, request, ApiResponseDto::class.java)
         val apiResponse = ApiResponse(response.body)
         modelRequest.apiResponse = apiResponse
-        val charge = chargeUser(modelRequest.oauthUser!!, apiResponse)
+        val charge = chargeUser(modelRequest.oauthUser !!, apiResponse)
         modelRequest.cost = charge
         modelRequestRepository.save(modelRequest)
         return response
     }
 
-    private fun chargeUser(user: OauthUser, response: ApiResponse): Float{
-        val requestCost = getPricePerToken(response.model!!) * response.usage?.totalTokens!!
+    private fun chargeUser(user: OauthUser, response: ApiResponse): Float {
+        val requestCost = getPricePerToken(response.model !!) * response.usage?.totalTokens !!
         user.availableCredits = user.availableCredits - requestCost
         userRepository.save(user)
         return requestCost
